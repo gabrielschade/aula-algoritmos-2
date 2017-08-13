4f060729fdc2a99d60e3886ee7018b1e80576bff
Gabriel Schade <gabriel.schade@hotmail.com>
2017-08-13 19:13:24 -0300

    Soluções dos exercícios da aula 3

--- AplicacaoCPP/AplicacaoCPP/AplicacaoCPP.cpp
+++ AplicacaoCPP/AplicacaoCPP/AplicacaoCPP.cpp
@@ -2,10 +2,90 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+void fibonacci()
+{
+	int primeiroValor = 0,
+		segundoValor = 1,
+		auxiliar,
+		limite;
+
+	cout << "Digite um número: ";
+	cin >> limite;
+	cout << "Série Fibonacci de " << limite << "." << endl;
+	cout << primeiroValor << ", " << segundoValor;
+
+	for (int contador = 0; contador < limite - 2; contador++) {
+		auxiliar = primeiroValor + segundoValor;
+		primeiroValor = segundoValor;
+		segundoValor = auxiliar;
+
+		cout << ", " << auxiliar;
+	}
+
+	cout << endl;
+	system("pause");
+}
+
+void calcularMediaEExibirAcima() {
+	const int TAMANHO = 10;
+	int arrayInteiro[TAMANHO] = { 0 };
+	int soma = 0,
+		media = 0;
+
+	cout << "Digite 10 valores: " << endl;
+	for (int posicao = 0; posicao < TAMANHO; posicao++) {
+		cin >> arrayInteiro[posicao];
+		soma += arrayInteiro[posicao];
+	}
+
+	media = soma / TAMANHO;
+	cout << endl << "Valores que estão acima da média: (" << media << ")" << endl;
+	for (int posicao = 0; posicao < TAMANHO; posicao++)
+		if (arrayInteiro[posicao] > media)
+			cout << arrayInteiro[posicao] << ", ";
+
+	cout << endl;
+	system("pause");
+}
+
+void diagonalPrincipalComUm() {
+	const int	
+		LINHAS = 3,
+		COLUNAS = 3;
+
+	int matriz[3][3] = { 0 };
+
+	for (int linha = 0; linha < LINHAS; linha++)
+		for (int coluna = 0; coluna < COLUNAS; coluna++)
+			if (linha == coluna)
+				matriz[linha][coluna] = 1;
+			else
+				matriz[linha][coluna] = 0;
+
+	for (int linha = 0; linha < LINHAS; linha++) {
+		cout << " | ";
+
+		for (int coluna = 0; coluna < COLUNAS; coluna++)
+			cout << matriz[linha][coluna] << " | ";
+
+		cout << endl;
+	}
+		
+	cout << endl;
+	system("pause");
+}
 
 int main()
 {
-    return 0;
+	setlocale(LC_ALL, "Portuguese");
+	//fibonacci();
+	//calcularMediaEExibirAcima();
+	//diagonalPrincipalComUm();
+	return 0;
 }
 
