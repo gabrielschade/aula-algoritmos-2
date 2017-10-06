4eb14bd24eb89784b5ffe97e9771d7daa4747d62
Gabriel Schade <re035148@resource.com.br>
2017-10-06 10:42:17 -0300

    Criação do repositório com o projeto para o trabalho de flood

---
+++ Flood/Flood/Flood.cpp
@@ -0,0 +1,24 @@
+// Flood.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "Flood.h"
+
+using namespace std;
+
+int main()
+{
+	escreverUmDosCampos(TAMANHO_PEQUENO);
+	cout << endl;
+	
+	escreverUmDosCampos(TAMANHO_MEDIO);
+	cout << endl;
+
+	escreverUmDosCampos(TAMANHO_GRANDE);
+	cout << endl;
+
+	system("pause");
+    return 0;
+}
+
